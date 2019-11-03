8f1fa85957bc024625446b63a3295a139a3b2693
Pietrotm <pietrotm@outlook.com>
2019-11-03 16:45:12 -0300

    Projeto iniciado e criado projetos.cs

---
+++ Lista.CursoCsharp/Produtos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista.CursoCsharp
+{
+    class Produtos
+    {
+        private string Nome;
+        private double Preco;
+        private int Quantidade;
+        public void Add(string nome, double preco, int quantidade = 0)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string ToText()
+        {
+            string resultado;
+            return resultado = Nome + ", " + Preco + ", " + Quantidade;
+        }
+
+        public string Noome
+        {
+            get { 
+                return Nome; 
+            }
+            set { 
+                if (value.Length > 1) Nome = value; 
+            }
+        }
+
+        public double Prreco {
+            set {
+                if (value >= 0) Preco = value; 
+            }
+            get {
+                return Preco; 
+            }
+        }
+
+        public int Quuantidade {
+            set {
+                
+                if (Quantidade >= 0) Quantidade = value; }
+            get {
+                return Quantidade; 
+            }
+        }
+
+    }
+}
---
+++ Lista.CursoCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista.CursoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos p = new Produtos();
+            //
+            Console.Write("Digite o nome do produto: ");
+            p.Noome = Console.ReadLine();
+            //
+            Console.Write("Digite o preço do produto: ");
+            p.Prreco = double.Parse(Console.ReadLine());
+            //
+            Console.Write("Digite a quantidade do produto: ");
+            p.Quuantidade = int.Parse(Console.ReadLine());
+            //
+            Console.WriteLine("\n {0}", p.ToText());
+        }
+    }
+}
